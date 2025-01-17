b41d4a04d297f550bbb81428ada8d4e1f66cba80
sputh-the-pigeon <spuththepigeon@gmail.com>
2025-01-17 20:06:27 +0530

    i2s mic working @ 16bit 16Khz sampling finally fu....

--- main/audio/sink.c
+++ main/audio/sink.c
@@ -3,6 +3,7 @@
 
 //#include "driver/i2s_std.h"
 #include "driver/i2s.h"
+#include "driver/uart.h"
 
 #include "esp_bt.h"
 #include "esp_gap_bt_api.h"
@@ -139,27 +140,32 @@ void esp_a2d_cb(esp_a2d_cb_event_t event, esp_a2d_cb_param_t* param) {
 void recv_audio(const uint8_t* buf, uint32_t len) {
     if (buf == NULL || len == 0) return;
     //i2s_channel_write(*tx_chan, buf, len, NULL, pdMS_TO_TICKS(20));
+    i2s_write(I2S_NUM_1, buf, len, NULL, pdMS_TO_TICKS(10));
 };
 
 static uint32_t hf_send_audio(uint8_t *buf, uint32_t len)
 {
-    int32_t data[len / 4];
+    if(buf == NULL || len < 0) return 0;
+    //int16_t data[len / 2];
     size_t num_read;
-    i2s_read(I2S_NUM_0, data, len, &num_read, portMAX_DELAY);
-    for(int i = 0; i < num_read / 4; i++) {
-        int16_t final_v = data[i] >> 11;
-        //int16_t final_d = (temp > INT16_MAX) ? INT16_MAX : (temp < -INT16_MAX) ? -INT16_MAX : (int16_t)temp;
-
-        memcpy(buf + (i * 2), &final_v, 2);
-    };
+    i2s_read(I2S_NUM_0, buf, len, &num_read, pdMS_TO_TICKS(10));
+    /*for(int i = 0; i < num_read / 2; i++) {
+        //uint16_t temp = data[i] >> 14;
+        //uint16_t final_d = (temp > INT16_MAX) ? INT16_MAX : (temp < -INT16_MAX) ? -INT16_MAX : (uint16_t)temp;
+        //final_d = ((final_d & 0xff) << 8) | ((final_d & 0xff00) >> 8);
+        uint8_t* ft = &temp;
+        buf[i * 2] = ft[0];
+        buf[i * 2 + 1] = ft[1]; 
+        memcpy(buf + (i * 2), data + i, 2);
+    };*/
     //i2s_channel_read(*rx_chan, buf, len, NULL, portMAX_DELAY);
     return len;
 }
 
 static void hf_recv_audio(const uint8_t *buf, uint32_t len)
 {
+    i2s_write(I2S_NUM_1, buf, len, NULL, pdMS_TO_TICKS(10));
     esp_hf_client_outgoing_data_ready();
-    //printf("%ld\n", len);
 }
 
 void hf_client_cb(esp_hf_client_cb_event_t event, esp_hf_client_cb_param_t *param) {
@@ -171,7 +177,7 @@ void hf_client_cb(esp_hf_client_cb_event_t event, esp_hf_client_cb_param_t *para
         if(param->conn_stat.state == ESP_HF_CLIENT_CONNECTION_STATE_SLC_CONNECTED) {
             esp_bt_gap_set_scan_mode(ESP_BT_NON_CONNECTABLE, ESP_BT_NON_DISCOVERABLE);
             printf("HFP client connected.\n");
-            esp_hf_client_connect_audio(param->conn_stat.remote_bda);
+            //esp_hf_client_connect_audio(param->conn_stat.remote_bda);
 
         } else if (param->conn_stat.state == ESP_HF_CLIENT_CONNECTION_STATE_DISCONNECTED) {
             esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
@@ -185,7 +191,9 @@ void hf_client_cb(esp_hf_client_cb_event_t event, esp_hf_client_cb_param_t *para
             //i2s_channel_enable(*rx_chan);
             esp_hf_client_outgoing_data_ready();
             printf("HFP Audio connected.\n");
+            uart_write_bytes(UART_NUM_2, ".incall", 7);
         } else if (param->audio_stat.state == ESP_HF_CLIENT_AUDIO_STATE_DISCONNECTED) {
+            uart_write_bytes(UART_NUM_2, ".endcall", 8);
             //i2s_channel_disable(*rx_chan);
         }
     }
--- main/main.c
+++ main/main.c
@@ -247,7 +247,7 @@ void app_main(void)
     };
     i2s_std_config_t i2s_rx_cfg = {
         .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(16000),
-        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO),
+        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
         .gpio_cfg = {
             .mclk = I2S_GPIO_UNUSED,
             .bclk = 32,
@@ -270,10 +270,10 @@ void app_main(void)
     i2s_config_t i2s_mic_config = {
         .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
         .sample_rate = 16000,
-        .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
         .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
         .communication_format = I2S_COMM_FORMAT_STAND_I2S,
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+        .intr_alloc_flags = 0,
         .dma_buf_count = 4,
         .dma_buf_len = 1024,
         .use_apll = false,
