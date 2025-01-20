f7ee0922faac13933cd69f5f977ccaa6f424e884
sputh-the-pigeon <spuththepigeon@gmail.com>
2025-01-20 12:52:39 +0530

    ...

--- main/main.c
+++ main/main.c
@@ -285,17 +285,18 @@ void app_main(void)
         .bck_io_num = 14,
         .ws_io_num = 12,
         .data_out_num = -1,
-        .data_in_num = 13
+        .data_in_num = 13,
+        .mck_io_num = -1,
     };
 
     i2s_config_t i2s_config = {
-        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_SLAVE),
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
         .sample_rate = 44100,
         .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
         .communication_format = I2S_COMM_FORMAT_STAND_MSB,
         .dma_buf_count = 4,
-        .dma_buf_len = 2048,
+        .dma_buf_len = 1024,
         .use_apll = false,
         .tx_desc_auto_clear = false,
         .fixed_mclk = 0
@@ -305,7 +306,8 @@ void app_main(void)
         .bck_io_num = 19,
         .ws_io_num = 18,
         .data_out_num = 5,
-        .data_in_num = -1
+        .data_in_num = -1,
+        .mck_io_num = -1,
     };
 
     i2s_driver_install(I2S_NUM_0, &i2s_mic_config, 0, NULL);
