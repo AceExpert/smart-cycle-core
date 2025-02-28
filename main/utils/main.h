2d28d4962d5cd82ae6d8ba8a771f8d7bbfcbcd1b
sputh-the-pigeon <spuththepigeon@gmail.com>
2025-03-01 01:04:49 +0530

    fix 1

--- main/utils/main.h
+++ main/utils/main.h
@@ -8,4 +8,4 @@ struct split_result {
 uint8_t match(const char* org, const char* new, int osize, int nsize);
 int split(const unsigned char* str, int len, char delim, struct split_result* res);
 void acked();
-int send_uart_cmd(uart_port_t port, void* src, size_t size);
+void send_uart_cmd(uart_port_t port, void* src, size_t size);
